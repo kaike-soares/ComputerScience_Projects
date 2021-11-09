759e0690663df1c731e7ba3495aeae7aa665ee96
Kaike <kaike.soares@hotmail.com>
2021-11-09 11:43:02 -0300

    Update: home.dart | Create: clima_model and clima_widget

--- MobileComputing/App_Weather_Map/lib/screens/home.dart
+++ MobileComputing/App_Weather_Map/lib/screens/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
+import 'package:http/http.dart' as http;
+import 'package:app_weather_map/model/clima_model.dart';
+import 'dart:convert';
 
 
 //Atalho: stf+tab
@@ -11,6 +14,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  late ClimaModel climaModel;
 
   List<String> _cidades = [
     "Aracaju",
@@ -45,6 +50,32 @@ class _HomeState extends State<Home> {
   //Cidade pré-selecionada
   String _cidadeSelecionada = "São Paulo";
 
+  carregaClima() async {
+    const String _apiURL = "api.openweathermap.org";
+    const String _path = "/data/2.5/weather";
+    const String _appid = "89d5a5d088b290f23ed3c6162554b04a"; //Chave de API
+    const String _units = "metric";
+    const String _lang = "pt_br";
+
+    final _parametros = {
+      "q" : _cidadeSelecionada,
+      "appid" : _appid,
+      "units" : _units,
+      "lang" : _lang
+    };
+
+    //Essa linha faz a requisição para a API externa
+    final climaResponse = await http.get(Uri.https(_apiURL, _path, _parametros));
+
+    //APENAS para testar:
+    //print("Url acessada: ${climaResponse.request!.url.toString()}");
+    //print(climaResponse.body);
+
+    if(climaResponse.statusCode == 200){
+      climaModel = ClimaModel.fromJson(jsonDecode(climaResponse.body));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -68,6 +99,7 @@ class _HomeState extends State<Home> {
               onChanged: (value){
                 setState(() {
                   _cidadeSelecionada = value!;
+                  carregaClima();
                 });
               },
             )
---
+++ MobileComputing/App_Weather_Map/lib/widgets/clima_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:app_weather_map/model/clima_model.dart';
+
+class ClimaWidget extends StatelessWidget {
+  const ClimaWidget({Key? key, required this.climaModel}) : super(key: key);
+
+  final ClimaModel climaModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
